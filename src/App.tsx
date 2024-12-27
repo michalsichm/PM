7ffedc7394d0385ceb83057a09ab2df9df0e420c
Michal <michalsichm@gmail.com>
2024-12-27 10:37:27 +0100

    set animation true

--- src/App.tsx
+++ src/App.tsx
@@ -51,7 +51,7 @@ const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonTabs>
-        <IonRouterOutlet animated={false}>
+        <IonRouterOutlet>
           <Route exact path="/pokedex">
             <Pokedex />
           </Route>
